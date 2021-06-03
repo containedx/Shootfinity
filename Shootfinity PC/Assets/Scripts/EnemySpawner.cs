f0bebbfe57f711770542b85fa06d59117add7d70
Kinga <k.zawarty@intermarum.pl>
2021-06-03 19:00:43 +0200

    spawning enemies

---
+++ Shootfinity PC/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public EnemyMovement enemyPrefab;
+    public Transform parent;
+    public GameObject player;
+
+    public float spawningSpeed;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnEnemy(spawningSpeed));
+    }
+
+    private IEnumerator SpawnEnemy(float time)
+    {
+        while (true)
+        {
+            var position = new Vector3(
+                Random.Range(-40, 40),
+                1,
+                Random.Range(-30, 30)
+            );
+            var enemy = Instantiate(enemyPrefab, position, Quaternion.identity, parent);
+            enemy.player = player;
+            Debug.Log("enemy spawned " + enemy.transform.position);
+            yield return new WaitForSeconds(time);
+        }
+    }
+}
